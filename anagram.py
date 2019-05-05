6eab6bade73cc3ca458c0c4a2c30ebc017aa5080
derlin-asp <daviddstacey@gmail.com>
2019-05-05 16:36:49 -0700

    Create anagram.py

---
+++ anagram.py
@@ -0,0 +1,26 @@
+from collections import OrderedDict
+
+
+Input = ['ram', 'act', 'arm', 'bat', 'cat', 'tab']
+
+def group_anagrams(items):
+    # TODO: Implement me
+    if items is None:
+        raise TypeError('Cannot be none')
+    if not items:
+        return items
+    listy = []
+
+    #sorting copy og items
+    for x in items:
+        y = sorted(x)
+        listy.append(  ( x,''.join(y) ) ) #making it a tuple of ( new , original )
+
+    listy = sorted(listy, key=lambda tup: tup[1])  #lookup sorted method    #sorting it by original version
+    print(listy)
+
+    res_list = [ x[0] for x in listy ]
+    return  res_list
+
+
+group_anagrams(Input)
